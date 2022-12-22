953ee751dbfdca742a630fd58cd7a8abec61f41c
usernameBakhtiyor <harhil46@gmail.com>
2022-12-22 10:02:29 +0500

    dfd

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :categories
   # get 'home/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
+  root "categories#index"
 end
