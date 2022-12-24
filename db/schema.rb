e157125488b9173c6028f608a31119bf7afe8991
usernameBakhtiyor <harhil46@gmail.com>
2022-12-24 22:33:44 +0500

    Update files

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_142043) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
@@ -39,7 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_142043) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe '/categories', type: :feature do
+  before :each do
+    @user = User.create(name: 'Bush', email: 'test@example.com', password: 'password',
+                        password_confirmation: 'password')
+    visit new_user_session_path
+    within('body') do
+      fill_in 'user_email', with: 'test@example.com'
+      fill_in 'Password', with: 'password'
+    end
+    click_button 'Log in'
+    sleep 3
+  end
+
+  it 'Check homepage content' do
+    expect(page.body).to include('Hello, Bush')
+    expect(page.body).to include('Add category')
+  end
+end
