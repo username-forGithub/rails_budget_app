e0da33832d3bd979489cbbd607c368d3d356920b
usernameBakhtiyor <harhil46@gmail.com>
2023-01-02 17:23:24 +0500

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :validatable
+         :recoverable, :validatable
 
   has_many :categories, dependent: :destroy
   # has_many :payments, through: :categories, dependent: :destroy
