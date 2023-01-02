b50c97660835fd13c161853af3b938ad3bb2d6ca
usernameBakhtiyor <harhil46@gmail.com>
2023-01-02 14:01:04 +0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
   resource :users do
     resources :categories do
       resources :payments
     end
   end
-
   authenticated :user do
     root "users#index"
   end
-
   devise_scope :user do
     root to: 'devise/sessions#splash', as: :splash_root
   end
